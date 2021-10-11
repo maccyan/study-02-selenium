822911b5538da15802d8d4f7568e8c514b9adf84
maccyan <macya21@gmail.com>
2021-10-11 17:03:41 +0900

    study02-02
    for文を使って、１ページ内の求人一覧を取得できるように改造してみましょう。
    また、会社名以外の項目として、求人タイトルも取得してください。

--- mynavi_sample.py
+++ mynavi_sample.py
@@ -6,8 +6,6 @@ import time
 import pandas as pd
 
 # Chromeを起動する関数
-
-
 def set_driver(driver_path, headless_flg):
     if "chrome" in driver_path:
           options = ChromeOptions()
@@ -37,11 +35,13 @@ def set_driver(driver_path, headless_flg):
 
 def main():
     search_keyword = "高収入"
-    # driverを起動
+        # driverを起動
     if os.name == 'nt': #Windows
         driver = set_driver("chromedriver.exe", False)
     elif os.name == 'posix': #Mac
         driver = set_driver("chromedriver", False)
+
+
     # Webサイトを開く
     driver.get("https://tenshoku.mynavi.jp/")
     time.sleep(5)
@@ -57,25 +57,45 @@ def main():
     # 検索ボタンクリック
     driver.find_element_by_class_name("topSearch__button").click()
 
-    # ページ終了まで繰り返し取得
-    # 検索結果の一番上の会社名を取得
-    name_list = driver.find_elements_by_class_name("cassetteRecruit__name")
-    
-    # 空のDataFrame作成
-    df = pd.DataFrame()
+    time.sleep(5)
+    # ポップアップを閉じる
+    driver.execute_script('document.querySelector(".karte-close").click()')
 
-    # 1ページ分繰り返し
-    print(len(name_list))
-    for name in name_list:
-        print(name.text)
-        # DataFrameに対して辞書形式でデータを追加する
-        df = df.append(
-            {"会社名": name.text, 
-             "項目B": "",
-             "項目C": ""}, 
-            ignore_index=True)
+    # ページ終了まで繰り返し取得
+    while True:
+        # 検索結果の一番上の会社名を取得
+        name_list = driver.find_elements_by_class_name("cassetteRecruit__name")
+    
+        # 求人タイトルの取得
+        name_title = driver.find_elements_by_xpath('//section/p/a')
+
+        next_link = []
+        for next_link in driver.find_elements_by_class_name("iconFont--arrowLeft"):
+            next = next_link.get_attribute("href")
+    
+
+    # 空のDataFrame作成
+#    df = pd.DataFrame()
+
+
+        print(len(name_list))
+        print(len(name_title)) 
+
+        for list,title in zip(name_list,name_title):
+            print(list.text,title.text)
+
+        driver.get(next)
         
+        if not next_link:
+            break    
         
+#        # DataFrameに対して辞書形式でデータを追加する
+#        df = df.append(
+#            {"会社名": name.text, 
+#             "項目B": "",
+#             "項目C": ""}, 
+#            ignore_index=True)
+      
 
 
 # 直接起動された場合はmain()を起動(モジュールとして呼び出された場合は起動しないようにするため)
